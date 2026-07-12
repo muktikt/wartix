9e1d236ec35983eb41d8195fe19daf82dc32638d
muktikt <rahayumukti200@gmail.com>
2026-07-12 18:51:24 +0700

    fix: responsive admin sidebar toggle classes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,7 +30,7 @@
     </div>
 
     {{-- SIDEBAR --}}
-    <aside class="fixed inset-y-0 left-0 z-40 w-56 bg-white border-r border-gray-100 flex flex-col flex-shrink-0 transition-transform duration-300 ease-in-out transform -translate-x-full md:translate-x-0 md:static"
+    <aside class="fixed inset-y-0 left-0 z-40 w-56 bg-white border-r border-gray-100 flex flex-col flex-shrink-0 transition-transform duration-300 ease-in-out transform md:static"
            :class="sidebarOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0'">
 
         {{-- Logo --}}
