2b9c25201c272ffec943060195b9f6a00e65dfdb
muktikt <rahayumukti200@gmail.com>
2026-07-12 18:54:09 +0700

    fix: make admin close menu button easier to tap and prevent event bubbling

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -44,8 +44,8 @@
                     Admin
                 </span>
             </div>
-            <button @click="sidebarOpen = false" class="text-gray-400 hover:text-gray-600 focus:outline-none md:hidden" title="Tutup Menu">
-                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <button @click.stop="sidebarOpen = false" class="text-gray-400 hover:text-gray-600 focus:outline-none md:hidden p-2 -mr-2 relative z-50" title="Tutup Menu">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
                 </svg>
             </button>
